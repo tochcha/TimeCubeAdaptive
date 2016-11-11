a6c8eb8081c8b985f74b0a06d676159e5bf71541
sasho <tochcha@gmail.com>
2016-11-11 19:31:19 +0200

    top-line

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
 $(document).ready(function () {
-	document.createElement('header');
-	document.createElement('nav');
-	document.createElement('section');
-	document.createElement('article');
-	document.createElement('aside');
-	document.createElement('footer');
-	document.createElement('figure');
-	document.createElement('figcaption');
-	document.createElement('main');
 
 	// подключение слайдера картинок
-	$('.main-slider').slick({
+	$('.zzzmain-slider').slick({
 		dots: true,
 		autoplay: true,
 		fade: true,
